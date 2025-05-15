01194d37ff6c4e97e02a0c5e1d20aa85250fecf6
Italo Maia <italo.fernandes.om@gmail.com>
2025-05-14 23:12:10 -0300

    implement vitest config

---
+++ backend/vitest.config.ts
@@ -0,0 +1,21 @@
+import path from 'path';
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+  test: {
+    globals: true,
+    environment: 'node',
+    root: './test',
+    coverage: {
+      provider: 'v8',
+      reporter: ['text', 'json', 'html'],
+      include: ['src/**/*.{ts,tsx}'],
+      exclude: ['node_modules', 'dist', 'coverage'],
+    },
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './'),
+    },
+  }
+});
