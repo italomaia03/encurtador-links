bb45375f8585ff424ecf3a6276a2ee50323fca08
Italo Maia <italo.fernandes.om@gmail.com>
2025-05-14 23:13:30 -0300

    test: impolement url service unit tests

---
+++ backend/test/unit/url.service.spec.ts
@@ -0,0 +1,105 @@
+import { URLService } from '@/services/url.service';
+import { UrlRepository } from '@/repositories/url.repository';
+import { envConfig } from '@/config/env.config';
+import { URLModel } from '@/models/url.model';
+import { afterEach, beforeEach, describe, expect, it, Mocked, vi } from 'vitest';
+import { nanoid } from 'nanoid';
+import { Prisma } from '@prisma/client';
+
+describe('URLService', () => {
+  let urlService: URLService;
+  let urlRepository: Mocked<UrlRepository>;
+  let urlCode: string;
+  let longUrl: string;
+
+  beforeEach(() => {
+    vi.mock('nanoid');
+    vi.mock('../models/url.model');
+    vi.mock('../config/env', () => ({
+      envConfig: {
+        DOMAIN_NAME: 'https://short.url',
+      },
+    }));
+
+    urlRepository = {
+      create: vi.fn(),
+      findByUrlCode: vi.fn(),
+    };
+
+    urlService = new URLService(urlRepository);
+
+    urlCode = 'abc123';
+    longUrl = 'https://example.com';
+  });
+
+  afterEach(() => {
+    vi.clearAllMocks();
+  });
+
+  describe('createShortUrl', () => {
+    it('should create a short URL', async () => {
+      const mockUrl = new URLModel({
+        longUrl,
+        shortUrl: `${envConfig.DOMAIN_NAME}/${urlCode}`,
+        urlCode: urlCode,
+      });
+
+      const mockCreatedUrl = {
+        id: 1,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+        longUrl,
+        shortUrl: `${envConfig.DOMAIN_NAME}/${urlCode}`,
+        urlCode,
+      };
+
+      vi.mocked(nanoid).mockReturnValue(urlCode);
+      urlRepository.create.mockResolvedValue(mockCreatedUrl);
+
+      const result = await urlService.createShortUrl(mockUrl.longUrl);
+
+      expect(nanoid).toHaveBeenCalledWith(8);
+      expect(urlRepository.create).toHaveBeenCalledWith(mockUrl);
+      expect(result).toEqual(mockCreatedUrl);
+    });
+
+    it('should throw an error if URL creation fails', async () => {
+      const mockError = new Prisma.PrismaClientKnownRequestError(
+        'Database error',
+        { code: 'P2002', clientVersion: '4.0.0' }
+      );
+      urlRepository.create.mockRejectedValue(mockError);
+
+      await expect(urlService.createShortUrl(longUrl)).rejects.toThrow(
+        'Database error'
+      );
+      expect(urlRepository.create).toHaveBeenCalled();
+    });
+  });
+
+  describe('getLongUrl', () => {
+    it('should retrieve the long URL', async () => {
+      const mockFoundUrl = {
+        id: 1,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+        longUrl,
+        shortUrl: `${envConfig.DOMAIN_NAME}/${urlCode}`,
+        urlCode,
+      };
+      urlRepository.findByUrlCode.mockResolvedValue(mockFoundUrl);
+      const result = await urlService.getLongUrl(urlCode);
+      expect(urlRepository.findByUrlCode).toHaveBeenCalledWith(urlCode);
+      expect(result).toEqual(longUrl);
+    });
+
+    it('should throw an error if URL not found', async () => {
+      urlRepository.findByUrlCode.mockResolvedValue(null);
+
+      await expect(urlService.getLongUrl(urlCode)).rejects.toThrow(
+        'URL not found'
+      );
+      expect(urlRepository.findByUrlCode).toHaveBeenCalledWith(urlCode);
+    });
+  });
+});
