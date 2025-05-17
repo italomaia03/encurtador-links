702a21e987e65217a0565dcfd431c114bf80e81d
Italo Maia <italo.fernandes.om@gmail.com>
2025-05-17 00:28:04 -0300

    feat: pages with react router dom

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,57 +1,21 @@
-import { useState, type FormEvent } from 'react';
-import { Header } from './components/Header';
-import { Footer } from './components/Footer';
-import { ShortenerForm } from './components/ShortnerForm';
-import { ShortenedUrlDisplay } from './components/ShortnedUrlDisplay';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { RedirectPage } from "./components/RedirectPage";
+import { ShortenerPage } from "./components/ShortenerPage";
 import "./App.css";
-
+import { Header } from "./components/Header";
+import { Footer } from "./components/Footer";
+import { ErrorPage } from "./components/ErrorPage";
 
 export default function App() {
-  const [originalUrl, setOriginalUrl] = useState('');
-  const [shortenedUrl, setShortenedUrl] = useState('');
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState('');
-
-  const handleSubmit = async (e: FormEvent) => {
-    e.preventDefault();
-    if (!originalUrl) {
-      setError('Por favor, insira um URL válido');
-      return;
-    }
-
-    setIsLoading(true);
-    setError('');
-
-    try {
-      await new Promise(resolve => setTimeout(resolve, 1000));
-      const randomString = Math.random().toString(36).substring(2, 8);
-      setShortenedUrl(`https://encurtador.com/${randomString}`);
-    } catch (err) {
-      setError('Ocorreu um erro ao encurtar o link. Tente novamente.');
-      console.log(err);
-    } finally {
-      setIsLoading(false);
-    }
-  };
-
   return (
-    <>
+    <Router>
       <Header />
-
-      <main className="flex-grow flex items-center justify-center p-4 bg-gray-100">
-        <div className="bg-white rounded-lg shadow-lg p-8 w-full max-w-md">
-          <ShortenerForm
-            originalUrl={originalUrl}
-            setOriginalUrl={setOriginalUrl}
-            isLoading={isLoading}
-            error={error}
-            handleSubmit={handleSubmit}
-          />
-          {shortenedUrl && <ShortenedUrlDisplay shortenedUrl={shortenedUrl} />}
-        </div>
-      </main>
-
+      <Routes>
+        <Route path="/" element={<ShortenerPage />} />
+        <Route path="/:shortId" element={<RedirectPage />} />
+        <Route path="/error" element={<ErrorPage />} />
+      </Routes>
       <Footer />
-    </>
+    </Router>
   );
 }
