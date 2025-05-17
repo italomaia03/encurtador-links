75b9f81963bcc13d621de8df5602b3cd57f375ec
Italo Maia <italo.fernandes.om@gmail.com>
2025-05-17 00:24:46 -0300

    feat: disable cors o local env

--- backend/app.ts
+++ backend/app.ts
@@ -1,11 +1,17 @@
 import express from 'express';
 import {router as urlRouter} from './routes/url.routes';
+import cors from 'cors';
 import {router as healthCheckRouter} from './routes/health.routes';
+import { envConfig } from './config/env.config';
 
 const app = express();
 
 app.use(express.json());
-app.use(urlRouter);
+if (envConfig.NODE_ENV === 'local'){
+  console.log(envConfig.NODE_ENV);
+  app.use(cors());
+}
+app.use("/api/v1",urlRouter);
 app.use(healthCheckRouter);
 
 export default app;
