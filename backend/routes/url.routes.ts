fc8de96c401ecfc01e6b37cd109e493185ed95cd
Italo Maia <italo.fernandes.om@gmail.com>
2025-05-19 23:35:53 -0300

    feat: add next parameter to routes

--- backend/routes/url.routes.ts
+++ backend/routes/url.routes.ts
@@ -1,4 +1,4 @@
-import { Request, Response, Router } from "express";
+import { NextFunction, Request, Response, Router } from "express";
 import { URLService } from "../services/url.service";
 import { UrlRepository } from "../repositories/url.repository";
 import { URLController } from "../controllers/url.controller";
@@ -8,5 +8,5 @@ const urlRepository = new UrlRepository();
 const urlService = new URLService(urlRepository)
 const urlController = new URLController(urlService);
 
-router.post("/shorten-url", (req: Request, res: Response) => urlController.createShortUrl(req, res));
-router.get("/:urlCode", (req: Request, res: Response) => urlController.getLongUrl(req, res));
+router.post("/shorten-url", (req: Request, res: Response, next: NextFunction) => urlController.createShortUrl(req, res, next));
+router.get("/:urlCode", (req: Request, res: Response, next: NextFunction) => urlController.getLongUrl(req, res, next));
