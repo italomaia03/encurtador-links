cf75a2761cda643e023903e5632be9af08d220d8
Italo Maia <italo.fernandes.om@gmail.com>
2025-05-20 00:09:53 -0300

    refactor: realocate logic to service layer

--- frontend/src/services/getLongUrlService.ts
+++ frontend/src/services/getLongUrlService.ts
@@ -1,7 +1,21 @@
+import { InternalServerError } from "../errors/internal-server.error";
+import { NotFoundError } from "../errors/not-found.error";
+
 export const getLongUrlService = async (shortId?: string) => {
   const apiURL = import.meta.env.VITE_APP_API_URL;
   if (!apiURL) {
     throw new Error("API URL is not defined");
   }
-  return await fetch(`${apiURL}/${shortId}`);
+  const data = await fetch(`${apiURL}/${shortId}`);
+  if (!data.ok) {
+    throw new NotFoundError("A URL não foi encontrada");
+  }
+
+  const { longUrl } = await data.json();
+
+  if (!longUrl) {
+    throw new InternalServerError("Ocorreu um erro inesperado");
+  }
+
+  return longUrl;
 };
