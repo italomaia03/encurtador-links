82cf92bfd9ea80ee9c20c25996ececc75c834902
Italo Maia <italo.fernandes.om@gmail.com>
2025-05-19 23:24:24 -0300

    fix: remove paths

--- backend/vitest.config.ts
+++ backend/vitest.config.ts
@@ -1,4 +1,3 @@
-import path from 'path';
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
@@ -13,9 +12,4 @@ export default defineConfig({
       exclude: ['node_modules', 'dist', 'coverage'],
     },
   },
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './'),
-    },
-  }
 });
