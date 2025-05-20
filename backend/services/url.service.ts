f06fbc832b046c88d46310da4c557f4aec429225
Italo Maia <italo.fernandes.om@gmail.com>
2025-05-19 23:39:21 -0300

    feat: adjust error handling to send custom errors

--- backend/services/url.service.ts
+++ backend/services/url.service.ts
@@ -1,37 +1,39 @@
-import { nanoid } from "nanoid";
-import { URLModel } from "../models/url.model";
-import { UrlRepository } from "../repositories/url.repository";
-import { envConfig } from "../config/env.config";
+import { nanoid } from 'nanoid';
+import { envConfig } from '../config/env.config';
+import { NotFoundError } from '../errors/not-found.error';
+import { URLModel } from '../models/url.model';
+import { UrlRepository } from '../repositories/url.repository';
+import { BadRequestError } from '../errors/bad-request.error';
+import { UrlAlreadyExistsError } from '../errors/url-already-exists.error';
 
 export class URLService {
   constructor(private readonly urlRepository: UrlRepository) {}
 
   async createShortUrl(longUrl: string) {
+    if (!longUrl || !longUrl.trim().length) {
+      throw new BadRequestError('Long URL not provided');
+    }
+    const urlCode = nanoid(8);
+    const shortUrl = `${envConfig.DOMAIN_NAME}/${urlCode}`;
     try {
-      const urlCode = nanoid(8);
-      const shortUrl = `${envConfig.DOMAIN_NAME}/${urlCode}`;
-      const url = new URLModel({longUrl,
-        shortUrl,
-        urlCode,
-      });
+      const url = new URLModel({ longUrl, shortUrl, urlCode });
       const newUrl = await this.urlRepository.create(url);
       return newUrl;
     } catch (error) {
-      console.error('Error creating URL:', error);
-      throw error;
+      if (error instanceof UrlAlreadyExistsError) {
+        throw new BadRequestError('URL code already exists');
+      }
     }
   }
 
   async getLongUrl(urlCode: string) {
-    try {
-      const url = await this.urlRepository.findByUrlCode(urlCode);
-      if (!url) {
-        throw new Error('URL not found');
-      }
-      return url.longUrl;
-    } catch (error) {
-      console.error('Error retrieving URL:', error);
-      throw error;
+    if (!urlCode) {
+      throw new BadRequestError('URL code is required');
     }
+    const url = await this.urlRepository.findByUrlCode(urlCode);
+    if (!url) {
+      throw new NotFoundError('URL not found');
+    }
+    return url.longUrl;
   }
 }
