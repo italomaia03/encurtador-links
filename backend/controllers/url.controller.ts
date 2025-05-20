623dc917521dd5674cf2fd06d2d321e9325a6817
Italo Maia <italo.fernandes.om@gmail.com>
2025-05-19 23:28:29 -0300

    feat: redirect error handling to middleware

--- backend/controllers/url.controller.ts
+++ backend/controllers/url.controller.ts
@@ -1,40 +1,29 @@
-import { Request, Response } from 'express';
+import { NextFunction, Request, Response } from 'express';
 import { URLService } from '../services/url.service';
+import { BadRequestError } from '../errors/bad-request.error';
 
 export class URLController {
   constructor(private readonly urlService: URLService) {}
 
-  async createShortUrl(req: Request, res: Response) {
+  async createShortUrl(req: Request, res: Response, next: NextFunction) {
     try {
       const { longUrl } = req.body;
-      if (!longUrl) {
-        res.status(400).json({ error: 'Long URL is required' });
-        return;
-      }
       const newUrl = await this.urlService.createShortUrl(longUrl);
       res.status(201).json(newUrl);
     } catch (error) {
       console.error('Error creating short URL:', error);
-      res.status(500).json({ error: 'Internal Server Error' });
+      next(error)
     }
   }
 
-  async getLongUrl(req: Request, res: Response) {
+  async getLongUrl(req: Request, res: Response, next: NextFunction) {
     try {
       const { urlCode } = req.params;
-      if (!urlCode) {
-        res.status(400).json({ error: 'URL code is required' });
-        return;
-      }
       const longUrl = await this.urlService.getLongUrl(urlCode);
-      if (!longUrl) {
-        res.status(404).json({ error: 'URL not found' });
-        return;
-      }
       res.status(200).json({ longUrl });
     } catch (error) {
       console.error('Error retrieving long URL:', error);
-      res.status(500).json({ error: 'Internal Server Error' });
+      next(error);
     }
   }
 }
